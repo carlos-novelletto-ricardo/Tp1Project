abdbfa8db08420212b4bf3fcc3a182090075617c
carlos.9lletto <dudu.9lletto@gmail.com>
2020-03-18 08:55:17 -0300

    first commit

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <arpa/inet.h> 
+#include  <ctype.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <errno.h>
+#include <signal.h>
+
+int main(int argc, char *argv[])
+{
+
+}
+
+
+
